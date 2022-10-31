8abb04cc88700150925a1d9746ea29c399117d2e
15Santiago <santiagorodriy@gmail.com>
2022-10-31 08:03:15 -0600

    Updates

---
+++ precache-manifest.81c7b5daad6ae24b8e6e4ab1759ffe71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24b8e521d1e421c40d07",
+    "url": "/todo-list/static/css/main.850ff3d4.chunk.css"
+  },
+  {
+    "revision": "24b8e521d1e421c40d07",
+    "url": "/todo-list/static/js/main.24b8e521.chunk.js"
+  },
+  {
+    "revision": "1b7560eac72c3809bcb5",
+    "url": "/todo-list/static/js/1.1b7560ea.chunk.js"
+  },
+  {
+    "revision": "6f47f260b12c7ad9f72e",
+    "url": "/todo-list/static/js/runtime~main.6f47f260.js"
+  },
+  {
+    "revision": "823950247c830022141aed8d1f23bae4",
+    "url": "/todo-list/index.html"
+  }
+];
